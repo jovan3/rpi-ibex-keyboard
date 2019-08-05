04a965a071b27968b2c250c2f27fd99d1bc59c0b
Yovan Naumovski <jovan3.14@gmail.com>
2019-08-05 22:03:29 +0300

    cleanup

--- arduino/keyboard/keyboard.ino
+++ arduino/keyboard/keyboard.ino
@@ -1,6 +1,9 @@
 #include <Keypad.h>
 #include <Wire.h>
 
+const int PRESSED_OFFSET = 100;
+const int NOT_PRESSED_OFFSET = 0;
+
 const byte ROWS = 2; //four rows
 const byte COLS = 5; //three columns
 char keys[ROWS][COLS] = {
@@ -15,21 +18,18 @@ Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 unsigned long loopCount;
 unsigned long startTime;
 String msg;
-String wireMsg;
 
 #define SLAVE_ADDRESS 0x04
 int state = 0;
 int number = 0;
 char keyVal = 0;
-
+int currentPressedOffset = 0;
 
 void setup() {
   pinMode(10, OUTPUT);
 
   Wire.begin(SLAVE_ADDRESS);
-  Wire.onRequest(sendData1);
-  //  Wire.onReceive(receiveData);
-  Wire.onReceive(receiveEvent);
+  Wire.onRequest(sendData);
 
   Serial.begin(9600);
   loopCount = 0;
@@ -37,20 +37,7 @@ void setup() {
   msg = "";
 }
 
-volatile byte buf [32];
-char transmitBuf[32];
-
-
-// called by interrupt service routine when incoming data arrives
-void receiveEvent (int howMany)
-{
-  for (byte i = 0; i < howMany; i++)
-  {
-    buf [i] = Wire.read();
-  }  // end of for loop
-}  // end of receiveEvent
-
-void sendData1() {
+void sendData() {
   Wire.write(keyVal);
 }
 
@@ -61,11 +48,6 @@ void loop() {
     Serial.println(loopCount / 5);
     startTime = millis();
     loopCount = 0;
-
-    for (int i = 0; i < 32; i++) {
-      Serial.print(buf[i]);
-    }
-    Serial.println();
   }
 
   // Fills kpd.key[ ] array with up-to 10 active keys.
@@ -76,29 +58,29 @@ void loop() {
     {
       if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
       {
-        switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+        switch (kpd.key[i].kstate) {
           case PRESSED:
             msg = " PRESSED.";
-            break;
-          case HOLD:
-            msg = " HOLD.";
+            currentPressedOffset = PRESSED_OFFSET;
             break;
           case RELEASED:
             msg = " RELEASED.";
+            currentPressedOffset = NOT_PRESSED_OFFSET;
             break;
-          case IDLE:
-            msg = " IDLE.";
+          default:
+            continue;
         }
 
-        wireMsg = kpd.key[i].kchar + " " + msg;
-        wireMsg.toCharArray(transmitBuf, wireMsg.length());
+        Serial.print(kpd.key[i].kchar);
+        Serial.println(msg);
 
-        keyVal = kpd.key[i].kchar;
+        keyVal = kpd.key[i].kchar + currentPressedOffset;
+
+
+        digitalWrite(10, LOW);
+        delay(5);
+        digitalWrite(10, HIGH);
       }
     }
-
-    digitalWrite(10, HIGH);
-    digitalWrite(10, LOW);
-    digitalWrite(10, HIGH);
   }
-}  // End loop
+}
