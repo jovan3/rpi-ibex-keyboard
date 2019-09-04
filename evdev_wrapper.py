7527db3e0fba058829f30f6514570cd7a7fcba63
Yovan Naumovski <jovan3.14@gmail.com>
2019-09-04 13:00:22 +0300

    add support for fn keys for the evdev wrapper

--- evdev_wrapper.py
+++ evdev_wrapper.py
@@ -7,6 +7,9 @@ def uinput_key_ev_to_ecode(uinput_ev):
     _, code = uinput_ev
     return code
 
+def ecode_to_uinput_key_ev(ecode):
+    _, code = ecode
+    return (1, code)
 
 class EvdevInput:
     """ evdev wrapper around the uinput keyboard device. Some game emulators can't handle
@@ -14,15 +17,30 @@ events coming from python-uinput directly, but have no issues with evdev generat
 
     def __init__(self):
         self.uinput = UInput()
+        self.state = set()
 
     def do_i2c_code(self, code):
         event, key = event_util.code_to_event(code)
-
         ecode = uinput_key_ev_to_ecode(key)
+        
         if event == event_util.PRESSED:
-            self.press(ecode)
+            self.state.add(key)
+
+            if event_util.fn_pressed(self.state):
+                fn_keys_combo = event_util.keys_to_fn(self.state)
+
+                if fn_keys_combo:
+                    evdev_key_combo = [uinput_key_ev_to_ecode(k) for k in fn_keys_combo]
+                    for key in evdev_key_combo:
+                        self.press(key)
+                    for key in evdev_key_combo:
+                        self.release(key)
+            else:
+                self.press(ecode)        
+
         elif event == event_util.RELEASED:
             self.release(ecode)
+            self.state.remove(key)
 
 
     def press(self, key):
